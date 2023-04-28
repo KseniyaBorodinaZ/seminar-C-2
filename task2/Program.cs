4b7317560a7bd6b4a6cb308cbc12599372046652
Kseniya Borodina <dveri.korona@mail.ru>
2023-04-28 19:09:51 +0300

    сохранение

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите трехзначное число : ");
+string number = Console.ReadLine();
+int numberLenght = number.Length;
+
+if (numberLenght > 2)
+{
+    Console.WriteLine($"Третья цифра этого числа это: {number[2]}");
+}
+else
+{
+    Console.WriteLine("Это число меньше трехзначного");
+}
